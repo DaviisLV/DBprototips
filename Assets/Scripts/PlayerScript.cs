48df7c320f09bb70fa3e691c85a427445e6addeb
davis.abols <davis.abols@va.lv>
2017-12-20 16:22:38 +0200

    first

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+    public generator generator;
+    private Vector3 previousPlayerPosition;
+
+    // Use this for initialization
+    void Start () {
+             previousPlayerPosition = generator.GetGridPosition(transform.position);
+        }
+	
+	// Update is called once per frame
+	void Update () {
+      
+            Vector3 playerGridPosition = generator.GetGridPosition(transform.position);
+            if (!playerGridPosition.Equals(previousPlayerPosition))
+            {
+                generator.UpdateGrid(transform.position);
+                previousPlayerPosition = playerGridPosition;
+            }
+        }
+    }
+
---
+++ Assets/Scripts/generator.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class generator : MonoBehaviour {
+
+    public GameObject GroundPanel;
+    public Transform Player;
+    private int radiusAroundPlayer = 2;
+    private float panelSizeX;
+    private float panelSizeZ;
+    private List<Vector3>  Grid = new List<Vector3>();
+    private Vector3 startPosition;
+
+    public List<GameObject> ActiveGameObjects = new List<GameObject>();
+    private GameObject plane;
+
+    private void Awake()
+
+    {   //get start position
+        startPosition = Player.transform.position;  
+        //get panel size
+        panelSizeX = GroundPanel.GetComponent<Renderer>().bounds.size.x;
+        panelSizeZ = GroundPanel.GetComponent<Renderer>().bounds.size.z;
+        //ganerate platform around player on start
+        for (int z = -radiusAroundPlayer; z <= radiusAroundPlayer; z++)
+        {
+            for (int x = -radiusAroundPlayer; x <= radiusAroundPlayer; x++)
+            {
+                if (x * x + z * z < radiusAroundPlayer * radiusAroundPlayer)
+                {
+                    plane = Instantiate(GroundPanel);         
+                    plane.transform.position = new Vector3(startPosition.x + x * panelSizeX, 0, startPosition.z + z * panelSizeZ);
+                                      
+                    Grid.Add(new Vector3(startPosition.x + x * panelSizeX, 0, startPosition.z + z * panelSizeZ));
+                    ActiveGameObjects.Add(plane);
+                }
+            }
+        }    
+    }
+
+    private List<Vector3> GetPlayerRadiusCoordiates(Vector3 gridPosition)
+    {
+        List<Vector3> newCoordinates = new List<Vector3>();
+
+        for (int z = -radiusAroundPlayer; z <= radiusAroundPlayer; z++)
+        {
+            for (int x = -radiusAroundPlayer; x <= radiusAroundPlayer; x++)
+            {
+                if (x * x + z * z < radiusAroundPlayer * radiusAroundPlayer)
+                {
+                   Vector3 vector = new Vector3(startPosition.x + (gridPosition.x * panelSizeX) + x * panelSizeX, 0, startPosition.z + (gridPosition.z * panelSizeZ) + z * panelSizeZ);
+                    newCoordinates.Add(vector);           
+                }
+            }
+        }      
+        return newCoordinates;
+    }
+    public void add(Vector3 poz)
+    {
+        plane = Instantiate(GroundPanel);
+        plane.transform.position = poz;
+        ActiveGameObjects.Add(plane);
+        Grid.Add(poz);
+    }
+    public void remove(Vector3 poz)
+    {
+        GameObject responseP = ActiveGameObjects.Find(obj => obj.transform.position == poz);
+        ActiveGameObjects.Remove(responseP);
+        Destroy(responseP);
+    }
+    public void UpdateGrid(Vector3 PlayerPosition)
+    {
+         Vector3 gridPosition = GetGridPosition(PlayerPosition);
+         List<Vector3> newPositions = GetPlayerRadiusCoordiates(gridPosition); 
+
+         List<Vector3> positionsToRemove = Grid.Except(newPositions).ToList();
+         List<Vector3> positionsToGenerate = newPositions.Except(Grid).ToList();
+
+        Debug.Log("jaunas");
+        foreach (var position in positionsToGenerate)
+        {
+           // Debug.Log(position);
+            add(position);
+        }
+
+        Debug.Log("vecas");
+        foreach (var position in positionsToRemove)
+        {
+             remove(position);
+         //  Debug.Log(position);
+        }
+    }
+    public Vector3 GetGridPosition(Vector3 playerPosition)
+    {     
+        int x = (int)Mathf.Floor(playerPosition.x / panelSizeX);
+        int z = (int)Mathf.Floor(playerPosition.z / panelSizeZ);
+        Debug.Log(x +"/"+ z);
+        return new Vector3(x,0,z);
+    }
+}
